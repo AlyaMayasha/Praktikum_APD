9e06ca3e6712a0dadb24224096b8c212c581a42a
AlyaMayasha <alyamyshaa@gmail.com>
2024-09-27 15:08:28 +0800

    Add files via upload
    livecode pertemuan ke-4

---
+++ Pertemuan 4/2409106054_Alya Mayasha_B1_livecode4.py
@@ -0,0 +1,67 @@
+batas = 20
+for i in range(10, batas, 2):
+ #   print(f"perulangan ke-{i}")
+
+buah_banyak = ["apel","mangga","anggur"]
+for buah in buah_banyak:
+ #   print(buah)
+
+for baris in range(1,4):
+ print("baris ke", baris)
+   for kolom in range(1,4):
+ #   print(kolom,"kolom", end=" ")
+ulang = 0
+lagi = "yes"
+while lagi == "yes":
+  ulang +=1
+  print("mabar")
+  lagi = input("mabar lagi gk?")
+print("selesai mabar!")
+
+#print(F"perulangan terjadi sebanyak {ulang} kali")
+
+for i in range(1,10):
+  if i == 4:
+    break
+  else:
+    print(f"perulangan ke-{i}")
+
+buah_banyak = ["apel","mangga","anggur"]
+for buah in buah_banyak
+
+while true: 
+  ulang = input("mabar lagi: ")
+  if ulang == "gk":
+    break
+ # print("mabar lagi")
+
+for i in range(1,10):
+  if i % 2 == 1:
+    continue
+#  print(f"perulangan ke-{i}")
+
+for i in range(5):
+  for j in range(5):
+    print("*", end="")
+ print()
+
+for i in range(1,5):
+ # print("*" * i)
+
+while kesempatan > 0 :
+username = input("Masukkan username: ")
+password = input("Masukkan password: ")
+
+if username == "admin" and password == "admin#1234":
+  print("Berhasil login")
+  break
+else:
+  print("username atau password salah")
+  kesempatan -=1
+  print(f"kesempatan login tersisa {kesempatan} kali ")
+
+
+  
+
+
+
