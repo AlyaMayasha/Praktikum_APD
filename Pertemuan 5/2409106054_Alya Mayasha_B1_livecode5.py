ecc26e0772876430097b6ba4e4f7ad6affd4f73a
AlyaMayasha <alyamyshaa@gmail.com>
2024-10-04 15:14:38 +0800

    Add files via upload
    livecode 5

---
+++ Pertemuan 5/2409106054_Alya Mayasha_B1_livecode5.py
@@ -0,0 +1,60 @@
+#data = [1,2,3,4,5, true,[1,2,3],"Adi"]
+#data_2 = list((1,2,3,4,5))
+
+#print(type(data))
+#print(type(data_2))
+#print(data[6])
+
+#data = ["apel", "durian", "jeruk"]
+
+#print(len(data))
+#print(data)
+#print()
+#data.append("semangka")
+#data.append(true)
+#print(data)
+#print()
+#data.insert(0,"rambutan")
+#print(data)
+
+#data = ["apel", "durian", "jeruk"]
+#print(data*3)
+
+#print(data)
+#print()
+#data[0:2] = "anggur", "semangka"
+#print(data)
+#del data[0]
+#data.pop(1)
+#print(data)
+#nilai_hapus = data.pop(1)
+#data_mahasiswa = [
+#    ["060","ifnu"],["APD","ORSIKOM"] 
+#    ["065, adi"]
+#]
+
+#print(data_mahasiswa[0][2][0])
+
+#data_mahasiswa = [
+#    ["admin", "admin123", "Admin"]
+#    ["user", "user1234", "user123"]
+#]
+
+#buah = ["apel", "durian", "jeruk"]
+#index = 1
+#for data in buah:
+#for index in range(len(buah)): 
+#    print(data)
+#    print(f"data ke {indeks +1}")
+#    print(buah{indeks})
+#    print("="*10)
+
+#for index, in item in enumerate(data_mahasiswa)
+
+#buah = ["apel","durian","jeruk"]
+
+#buah1, *banyak_buah = buah
+#print(buah1)
+#print(banyak_buah)
+
+data_mahasiswa = ["ifnu", "adi", "ucup", "michael"]
